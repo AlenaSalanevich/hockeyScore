8f222891d142ef0379ecda8df2edc66c20ae69d9
Alena Salanevich <Alena.Salanevich@smarsh.com>
2020-01-03 17:31:44 +0300

    [module-1] add classes

--- hockey-score/src/app/core/model/team/team.ts
+++ hockey-score/src/app/core/model/team/team.ts
@@ -1,2 +1,16 @@
+import { User } from '../user/user';
+
 export class Team {
+
+    private _name: string;
+    private _users: User[];
+    private _place: number;
+
+
+constructor(name:string, users: User[], place:number){
+    this._name=name;
+    this._users=users;
+    this._place=place;
+}
+
 }
--- hockey-score/src/app/core/model/user/user.ts
+++ hockey-score/src/app/core/model/user/user.ts
@@ -1,2 +1,24 @@
 export class User {
+    private _id: number;
+    private _last_name: string;
+    private _first_name: string;
+
+
+    constructor(id: number, first_name: string, last_name:string){
+        this._id=id;
+        this._first_name=first_name;
+        this._last_name=last_name;
+    }
+
+    get first_name():string{
+        return this._first_name
+    }
+
+    get last_name():string{
+        return this._last_name;
+    }
+
+    get id(): number{
+        return this._id;
+    }
 }
