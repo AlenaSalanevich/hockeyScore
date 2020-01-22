de68814938ef3d6cde29cde5d49205efc294ab97
Alena Salanevich <Alena.Salanevich@smarsh.com>
2020-01-22 17:21:24 +0300

    [module-2] fix *ngFor in app-team.html

--- hockey-score/src/app/teams/team/team.component.ts
+++ hockey-score/src/app/teams/team/team.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { TeamService } from '../team.service';
 import { Team } from 'src/app/shared/model/team/team';
 import { Player } from 'src/app/shared/model/player/player';
+import { TeamListComponent } from '../team-list/team-list.component';
 
 @Component({
   selector: 'app-team',
@@ -9,8 +10,9 @@ import { Player } from 'src/app/shared/model/player/player';
   styleUrls: ['./team.component.css'],
 })
 export class TeamComponent implements OnInit {
-  @Input() public team: Team
-  players: Player[];
+
+  @Input() team: Team;
+  @Input() players: Player[];
   constructor() { }
   columnsToDisplay = ['number', 'name', 'position', 'country', 'age', 'shoots', 'height', 'weight'];
   ngOnInit() {
