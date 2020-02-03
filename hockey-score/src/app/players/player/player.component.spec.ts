a330966a00643b465cc5e5dca3f6111373376be7
AlenaSalanevich <Alena_Salanevich@epam.com>
2020-02-03 14:26:33 +0300

    Revert "[module-3] add  tests"
    This reverts commit 6a8194ba12d66a160ae5845e336c0ae366248b15.

--- hockey-score/src/app/players/player/player.component.spec.ts
+++ hockey-score/src/app/players/player/player.component.spec.ts
@@ -15,7 +15,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 describe('PlayerComponent', () => {
   let component: PlayerComponent;
   let fixture: ComponentFixture<PlayerComponent>;
-  let playerService: PlayerService;
   let playerServiceSpy: jasmine.SpyObj<PlayerService>;
   let stubPlayers: Player[];
   let getPlayersSpy: any;
@@ -47,12 +46,13 @@ describe('PlayerComponent', () => {
       providers: [{ provide: PlayerService, useValue: spy }]
     })
       .compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(PlayerComponent);
-    playerService = TestBed.inject(PlayerService);
     component = fixture.componentInstance;
     fixture.detectChanges();
-    
-  }));
+  });
 
   it('should create', () => {
 
